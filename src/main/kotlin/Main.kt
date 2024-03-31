fe54539ae1521d1765c1da710bb39bcf443b4e47
daniyaljavaid <daniyaljavaid95@gmail.com>
2024-04-01 02:16:39 +0500

    extensions refactoring

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -42,24 +42,3 @@ fun main() {
         }
 
 }
-
-
-fun String.removeSurroundingBraces() = removeSurrounding("(", ")")
-fun String.removeSurroundingQuotes() = removeSurrounding("\"", "\"")
-
-fun String.removePlatformPrefixIfExists() =
-    if (startsWith("platform")) removePlatformPrefix()
-    else
-        this
-
-fun String.removeImplementationPrefix() =
-    replace(".*implementation".toRegex(RegexOption.IGNORE_CASE), "")
-        .removeSurroundingBraces()
-        .removeSurroundingQuotes()
-
-fun String.removePlatformPrefix() =
-    replace(".*platform".toRegex(RegexOption.IGNORE_CASE), "")
-        .removeSurroundingBraces()
-        .removeSurroundingQuotes()
-
-
