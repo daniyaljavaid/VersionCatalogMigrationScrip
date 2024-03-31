f77b07f3754b945ebfee4fb9470dbccd6339e823
daniyaljavaid <daniyaljavaid95@gmail.com>
2024-03-31 22:23:18 +0500

    initial commit with catalog details extraction

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,65 @@
+fun main() {
+    val inputString = """
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation(platform("androidx.compose:compose-bom:2022.10.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2022.10.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    """
+
+    inputString.split("\n")
+        .filter { it.trim().isNotEmpty() }
+        .forEach {
+            println("==========================")
+
+            val filteredInput = it.removeImplementationPrefix().removePlatformPrefixIfExists()
+
+            val arr = filteredInput.split(":")
+
+            val group = arr[0]
+            val name = arr[1]
+
+            var versionString = ""
+
+            if (arr.size == 3)
+                versionString = ", version.ref = \"${arr[2]}\""
+
+            val output = "$name = { group = \"${group}\", name = \"${name}\"${versionString}}"
+
+            println(it.trim())
+            println(filteredInput)
+            println(output)
+        }
+
+}
+
+
+fun String.removeSurroundingBraces() = removeSurrounding("(", ")")
+fun String.removeSurroundingQuotes() = removeSurrounding("\"", "\"")
+
+fun String.removePlatformPrefixIfExists() =
+    if (startsWith("platform")) removePlatformPrefix()
+    else
+        this
+
+fun String.removeImplementationPrefix() =
+    replace(".*implementation".toRegex(RegexOption.IGNORE_CASE), "")
+        .removeSurroundingBraces()
+        .removeSurroundingQuotes()
+
+fun String.removePlatformPrefix() =
+    replace(".*platform".toRegex(RegexOption.IGNORE_CASE), "")
+        .removeSurroundingBraces()
+        .removeSurroundingQuotes()
+
+
